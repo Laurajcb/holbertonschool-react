609b10383dadff3265433e2ca037628abfcf3560
Laurajcb <callejaslaura96@gmail.com>
2022-05-18 11:53:21 -0500

    Added task 0 tests OK

---
+++ 0x03-React_component/task_0/dasboard/src/App/App.js
@@ -0,0 +1,48 @@
+import React, { PureComponent } from "react";
+import { Notifications } from "../Notifications/Notifications";
+import { getLatestNotification } from "../utils/utils";
+import { Header } from "../Header/Header";
+import { Login } from "../Login/Login";
+import { Footer } from "../Footer/Footer";
+import CourseList from "../CourseList/CourseList";
+import { bool } from "prop-types";
+
+// Implement class Components
+class App extends PureComponent {
+  render() {
+    // Assign props to local variables
+    const { isLoggedIn } = this.props;
+
+    const listCourses = [
+      { id: 1, name: "ES6", credit: 60 },
+      { id: 2, name: "Webpack", credit: 20 },
+      { id: 3, name: "React", credit: 40 },
+    ];
+
+    const listNotifications = [
+      { id: 1, type: "default", value: "New course available" },
+      { id: 2, type: "urgent", value: "New resume available" },
+      { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
+    ];
+    return (
+      <>
+        <Notifications listNotifications={listNotifications} />
+        <div className="App">
+          <Header />
+          {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          <Footer />
+        </div>
+      </>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: bool,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+};
+
+export default App;
