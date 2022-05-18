d16fbc4f6c951ea0b6658e3a0895a4c078ddc3d6
Laurajcb <callejaslaura96@gmail.com>
2022-05-18 17:08:26 -0500

    trying fix for app

--- 0x03-React_component/task_0/dasboard/src/App/App.js
+++ 0x03-React_component/task_0/dasboard/src/App/App.js
@@ -1,53 +1,47 @@
-import React, { PureComponent } from "react";
+import React, { Component } from "react";
 import { Notifications } from "../Notifications/Notifications";
-import { getLatestNotification } from "../utils/utils";
 import { Header } from "../Header/Header";
 import { Login } from "../Login/Login";
 import { Footer } from "../Footer/Footer";
 import CourseList from "../CourseList/CourseList";
-import { bool } from "prop-types";
 import PropTypes from "prop-types";
-
-// Assign props to local variables
-const { isLoggedIn } = this.props;
+import { bool } from "prop-types";
+import { getLatestNotification } from "../utils/utils";
 
 const listCourses = [
   { id: 1, name: "ES6", credit: 60 },
   { id: 2, name: "Webpack", credit: 20 },
   { id: 3, name: "React", credit: 40 },
 ];
-
 const listNotifications = [
   { id: 1, type: "default", value: "New course available" },
   { id: 2, type: "urgent", value: "New resume available" },
   { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
 ];
-// Implement class Components
+
 class App extends Component {
   constructor(props) {
     super(props);
   }
-
   render() {
+    const { isLoggedIn } = this.props;
     return (
-      <>
+      <Fragment>
         <Notifications listNotifications={listNotifications} />
-        <div className="App">
-          <Header />
-          {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
-          <Footer />
-        </div>
-      </>
+        <Header />
+        {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+        <Footer />
+      </Fragment>
     );
   }
 }
 
-App.propTypes = {
-  isLoggedIn: bool,
-};
-
 App.defaultProps = {
   isLoggedIn: false,
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
 };
 
 export default App;
