0ea1089a040177458e3c3c7aae98e8a2ca3ce3a0
Laurajcb <callejaslaura96@gmail.com>
2022-05-19 09:45:12 -0500

    fix boble call of the object __html:

--- 0x03-React_component/task_0/dashboard/src/Notifications/NotificationItem.js
+++ 0x03-React_component/task_0/dashboard/src/Notifications/NotificationItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 function NotificationItem(props) {
     return (
         <li data-notification-type={props.type} dangerouslySetInnerHTML={props.html}>
-        {props.value}
+            {props.value}
         </li>
     );
 }
